9ec470225b98b8acca0a5b7b9a8fd0d5945dfa0f
Woo KyungJun <natcho9010@gmail.com>
2022-11-05 21:20:54 +0900

    feat: add Loading Page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes as ReactRouterRoutes, Navigate } from 'react-router-dom';
 import { Home } from '@/pages/Home';
+import { Loading } from '@/pages/Loading';
+import { Quiz } from '@/pages/Quiz';
 
 function Router() {
   return (
     <ReactRouterRoutes>
       <Route path="/" element={<Home />} />
+      <Route path="/loading" element={<Loading />} />
+      <Route path="/quiz/[id]" element={<Quiz />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </ReactRouterRoutes>
   );
---
+++ src/pages/Loading.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+
+import { useGetQuiz } from '@/hooks/useGetQuiz';
+import useInterval from '@/hooks/useInterval';
+
+function Loading() {
+  const [dots, setDots] = useState('.');
+  const navigate = useNavigate();
+
+  const {
+    data, isFetching, isFetched,
+  } = useGetQuiz();
+
+  useInterval(() => {
+    if (isFetching) {
+      setDots((prev) => {
+        if (prev.length >= 3) {
+          return '.';
+        }
+
+        return prev.concat('.');
+      });
+    }
+  }, isFetching ? 300 : null);
+
+  useEffect(() => {
+    if (isFetched && data) {
+      navigate('quiz/1');
+    }
+  }, [isFetched, navigate, data]);
+
+  return <div className="w-full h-full flex justify-center items-center">{`Loading${dots}`}</div>;
+}
+
+export { Loading };
