59b5d4cddcff6ba7501f3769db36e68391b8a857
Woo KyungJun <natcho9010@gmail.com>
2022-11-06 23:16:06 +0900

    feat: change button content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ function Home() {
     <div className="flex flex-col items-center w-full h-full">
       <h1 className="flex justify-center items-end w-full h-[30%]">Classting Quiz!</h1>
       <div className="flex justify-center items-center flex-1">
-        <Link className="flex justify-center items-center font-bold w-fit h-[1.5em] rounded-xl text-[40px] px-[10px] bg-red-300 hover:translate-y-[-10%] transition-transform" href="/loading">시작하기</Link>
+        <Link className="flex justify-center items-center font-bold w-fit h-[1.5em] rounded-xl text-[40px] px-[10px] bg-red-300 hover:translate-y-[-10%] transition-transform" href="/loading">퀴즈 풀기</Link>
       </div>
     </div>
   );
