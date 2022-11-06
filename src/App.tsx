d375850183ee7fab47c697dc255d267d65603242
Woo KyungJun <natcho9010@gmail.com>
2022-11-06 22:35:57 +0900

    style: scroll style in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Router } from './Router';
 
 function App() {
   return (
-    <div className="flex justify-center w-full h-full">
-      <main className="max-w-[600px] w-full h-full">
+    <div className="flex justify-center w-full h-full overflow-y-scroll scrollbar-hide">
+      <main className="max-w-[600px] w-full">
         <Router />
       </main>
     </div>
