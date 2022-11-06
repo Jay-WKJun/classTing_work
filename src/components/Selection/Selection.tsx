d9de6e0d2a74c395816e8255183516dc9b14f651
Woo KyungJun <natcho9010@gmail.com>
2022-11-06 20:34:33 +0900

    feat: add Selection correct incorrect

--- src/components/Selection/Selection.tsx
+++ src/components/Selection/Selection.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import classNames from 'classnames';
 
 import styles from './Selection.module.scss';
 
 interface SelectionProps {
   content: string;
+  className?: string;
+  state?: 'correct' | 'inCorrect' | null;
+  onClick?: MouseEventHandler;
 }
 
 function Selection({
   content,
+  className,
+  state,
+  onClick,
 }: SelectionProps) {
   return (
-    <button type="button" className={classNames('flex items-center w-full px-[20px] py-[5px] border-[2px] border-black rounded-full cursor-pointer font-bold', styles.button)}>
-      <div className="w-[15px] h-[15px] mr-[10px] rounded-full content-none bg-black" />
+    <button
+      type="button"
+      className={classNames('flex items-center w-full px-[20px] py-[5px] border-[2px] border-black rounded-full cursor-pointer font-bold', styles.wrapper, { [styles.correct]: state === 'correct' }, { [styles.incorrect]: state === 'inCorrect' }, className)}
+      onClick={onClick}
+    >
+      <div className={classNames('w-[15px] h-[15px] mr-[10px] rounded-full content-none', styles.button)} />
       <span>{content}</span>
     </button>
   );
