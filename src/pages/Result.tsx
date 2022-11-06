d9e0f666365e68bce718837ea5739c0534acedb9
Woo KyungJun <natcho9010@gmail.com>
2022-11-06 22:36:13 +0900

    feat: render result in Result page

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,12 +1,46 @@
+import { QuizSelections } from '@/components/QuizSelections/QuizSelections';
 import { useGetQuiz } from '@/hooks/useGetQuiz';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
 function Result() {
-  const { data: quizs } = useGetQuiz({ isFinished: true });
+  const navigate = useNavigate();
+  const {
+    data: quizs, isError, isFetching,
+  } = useGetQuiz({ isFinished: true });
 
-  console.log(quizs);
+  useEffect(() => {
+    if (isError || (!isFetching && !quizs)) {
+      navigate('/');
+    }
+  }, [navigate, isError, isFetching, quizs]);
 
-  return <div>result</div>;
+  return (
+    <div className="w-full py-[100px]">
+      <section className="flex flex-col justify-around items-center h-[150px] mb-[100px]">
+        <h1>정답률</h1>
+        <h1>
+          {`${quizs?.results.reduce((prev, curr) => {
+            if (curr.isCorrect()) {
+              return prev + 1;
+            }
+
+            return prev;
+          }, 0)} / ${quizs?.results.length}`}
+
+        </h1>
+      </section>
+      {
+        quizs
+          ? quizs.results.map((quiz) => (
+            <div className="w-full h-[400px] mb-[50px]">
+              <QuizSelections isSelected quiz={quiz} />
+            </div>
+          ))
+          : <div>loading</div>
+      }
+    </div>
+  );
 }
 
 export { Result };
