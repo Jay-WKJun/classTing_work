1e975a46bf0baa86989f4255e18dcebdfb8d6b0c
Woo KyungJun <natcho9010@gmail.com>
2022-11-06 20:35:19 +0900

    feat: show Quiz result

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,13 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+
 import { Selection } from '@/components/Selection/Selection';
 import { useGetQuiz } from '@/hooks/useGetQuiz';
-import React from 'react';
-import { useParams } from 'react-router';
+import { Selection as SelectionModel } from '@/models/Selection';
+
+function getSelectionState(selection: SelectionModel, isSelected: boolean) {
+  const state = selection.getThisSelectionState();
+
+  if (isSelected && selection.isCorrect) {
+    return 'correct';
+  }
+
+  if (state === 'not selected') {
+    return null;
+  }
+
+  return state;
+}
 
 function Quiz() {
+  const [isSelected, setIsSelected] = useState(false);
+  const navigate = useNavigate();
   const params = useParams();
   const { id } = params;
 
-  const { currentQuiz } = useGetQuiz({ index: Number(id) });
+  const { currentQuiz, data, isFetched } = useGetQuiz({ index: Number(id) });
+
+  useEffect(() => {
+    if (isFetched && !data) {
+      navigate('/');
+    }
+  }, [isFetched, data, navigate]);
 
   return (
     <div className="flex flex-col w-full h-full">
@@ -17,8 +41,16 @@ function Quiz() {
       <section className="flex-1 flex justify-center items-center">
         <div className="flex flex-col child/Wo.first:mt-[20px]">
           {
-            currentQuiz?.selections.map((selection) => (
-              <Selection content={selection} />
+            currentQuiz?.selections.map((selection, i) => (
+              <Selection
+                content={selection.content}
+                state={getSelectionState(selection, isSelected)}
+                onClick={() => {
+                  selection.selectThis();
+                  currentQuiz.selectIndex(i);
+                  setIsSelected(true);
+                }}
+              />
             ))
           }
         </div>
