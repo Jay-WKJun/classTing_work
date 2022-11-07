a186a522314ef47c6e3678bc9e97de008bc5edb3
Woo KyungJun <natcho9010@gmail.com>
2022-11-07 22:49:26 +0900

    feat: add BarChart

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { format } from 'date-fns';
 
@@ -7,6 +7,7 @@ import { QuizSelections } from '@/components/QuizSelections/QuizSelections';
 import { useGetQuiz } from '@/hooks/useGetQuiz';
 import { useGlobalContext } from '@/store/GlobalContext';
 import type { QuizModel } from '@/models/QuizModel';
+import { BarChart } from '@/components/BarChart/BarChart';
 
 function getCounts(callback: (el: QuizModel) => boolean, quizs?: QuizModel[]) {
   if (!quizs) { return 0; }
@@ -48,10 +49,26 @@ function Result() {
 
   const SpentTime = format(getSpentTime(), '소요시간 : m분 s초');
 
+  const data = [
+    {
+      name: 'result',
+      bars: [{
+        key: 'correct',
+        color: '#8884d8',
+        value: correctCount,
+      }, {
+        key: 'inCorrect',
+        color: '#de3c13',
+        value: inCorrectCount,
+      }],
+    },
+  ];
+
   return (
     <div className="w-full py-[100px]">
-      <section className="flex flex-col justify-around items-center h-[200px] mb-[50px]">
-        <h1 className="mb-[10px]">결과</h1>
+      <section className="flex flex-col justify-around items-center h-fit mb-[50px]">
+        <h1 className="mb-[30px]">결과</h1>
+        <BarChart width={300} height={300} data={data} />
         <h3>
           {CorrectCount}
         </h3>
@@ -62,18 +79,18 @@ function Result() {
           {SpentTime}
         </h3>
       </section>
-      <section className="flex flex-col gap-[50px]">
+      <section className="flex flex-col gap-[50px] mb-[50px]">
         {
           quizs
             ? quizs.results.map((quiz) => (
-              <div className="w-full h-[400px] mb-[50px]">
+              <div className="w-full min-h-[400px]">
                 <QuizSelections hold isSelected quiz={quiz} />
               </div>
             ))
             : <div>loading</div>
         }
       </section>
-      <footer className="flex flex-col justify-center items-center w-full h-[200px] child/Wo.last:mb-[30px]">
+      <footer className="flex flex-col justify-center items-center w-full min-h-[200px] child/Wo.last:mb-[30px]">
         <Link
           href="/quiz/0"
           className="flex justify-center items-center px-[20px] py-[10px] font-bold text-[30px] rounded-2xl bg-lime-500"
